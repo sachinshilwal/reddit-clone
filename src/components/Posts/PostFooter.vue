f87adfe5882e5d2f785c659909f2c4b0235299d7
Sachin Shilwal <34100453+sachinbro@users.noreply.github.com>
2022-02-04 22:38:57 +0545

    top 30 comments are shown

--- src/components/Posts/PostFooter.vue
+++ src/components/Posts/PostFooter.vue
@@ -1,29 +1,45 @@
 <template>
     <div class="post__item">
+      <div @click="$emit('getCommentTree',)">
           <i class="fas fa-comment-alt"></i>
-          <span>{{ post.num_comments }} comments</span>
-        </div>
-        <div class="post__item">
+          <span>{{ post.num_comments }} comments </span>
+      </div>
+    
           <i class="fas fa-gift"></i>
           <span>Give Award</span>
-        </div>
-        <div class="post__item">
+    </div>
           <i class="fas fa-share"></i>
           <span>Share</span>
-        </div>
-        <div class="post__item">
+      
+        
           <i class="fas fa-bookmark"></i>
           <span>Save</span>
-        </div>
+        
         <div class="post__item">
           <i class="fas fa-ellipsis-h"></i>
-        </div>
+        
         <div class="upvote-ratio">
           <span>{{ post.upvote_ratio * 100 }}% upvoted</span>
         </div>
+    </div>
+        
 </template>
 <script>
     export default {
         props: ['post'],
+        emits: ['getCommentTree'],
+       
+        data(){
+          return{
+            comments: [],
+            showComment: false,
+          }
+        },
     }
 </script>
+
+<style lang="scss">
+span::after{
+  content: " " !important;
+}
+</style>
