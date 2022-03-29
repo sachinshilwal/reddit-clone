d16308abda8865a9f13a7c7092e58f75a995793b
Sachin Shilwal <34100453+sachinbro@users.noreply.github.com>
2022-03-29 13:08:55 +0545

    minor fix related to authorization header: 2 separate header for production and development

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,9 +4,17 @@
 </template>
 
 <script>
+import 'dotenv/config'
     export default {
         created(){
+            let b = new Buffer.from(`${process.env.VUE_APP_client_id}:${process.env.VUE_APP_client_state}`,'base64').toString('ascii')
+            console.log(b)
+            
             this.getInfo()
+            
+            
+        },
+        mounted(){
             
         },
         methods:{
@@ -17,13 +25,13 @@
                  let data = qs.stringify({
                     'grant_type': 'authorization_code',
                     'code': `${this.$route.query.code}`,
-                    'redirect_uri': 'http://localhost:8080'
+                    'redirect_uri': process.env.VUE_APP_redirecturi
                 });
                 let  config = {
                     method: 'post',
                     url: 'https://www.reddit.com/api/v1/access_token',
                     headers: {
-                        'Authorization': 'Basic djI4NlU4VkVRQ3RpTG9FREhheG5HUTpWYVM2b3hIU20xa1JuQUliMGRBWDVuZC1hYlJlNnc=',
+                        'Authorization': 'Basic '+ process.env.VUE_APP_authorization,
                         'Content-Type': 'application/x-www-form-urlencoded',
                         'Cookie': 'csv=2; edgebucket=s1NxhNpboePdOeyY0J; loid=0000000000cbippo4k.2.1621917136574.Z0FBQUFBQmg5akRDZEVnb3BEWEp2RThIQ0k5OFcydGI0QVdJRlZBWlR3ZGdsODc2U0I3QlhYdDVKd3dXWkVGWUUtUnZjdmt2elVkbjVUcDQ0Z2RUaDhVSlA2Vk9abms4aHVFYkJoTUp0WERKMVFUS1pKT2VtZ1RYcDZlMk1hd081MzBOWC0tdEZjeEE; token_v2=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDMyODk0OTcsInN1YiI6Ii1GTk1iUWdKVXE0ZDNWSmM4REM3Z2l3Q1RWRkF4ZWciLCJsb2dnZWRJbiI6ZmFsc2UsInNjb3BlcyI6WyIqIiwiZW1haWwiLCJwaWkiXX0.k3seK7WzeR5BTPieZFoIODMFd6EqxvO2LVrY4jR9_bQ'
                     },
